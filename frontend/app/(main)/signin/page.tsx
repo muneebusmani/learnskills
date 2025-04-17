50e05c17736bda96f964edbabce918ce9b37c3b4
muneeb <muneebusmani8355@gmail.com>
2025-04-17 19:12:15 +0500

    modularizing

--- frontend/app/(main)/signin/page.tsx
+++
@@ -1,9 +0,0 @@
-import { JSX } from "react";
-import { SignIn } from "./signin";
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Learn Skills | Sign In",
-};
-const Page = (): JSX.Element => <SignIn />;
-export default Page;
